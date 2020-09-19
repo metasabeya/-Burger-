b17bfae838c50a5d7afd94038cc34a1835b1b4fb
unknown <metybuza46@gmail.com>
2020-09-18 18:24:19 -0600

    added different routes to it

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,6 +7,8 @@ router.get("/", function(req, res) {
         var hbsObject = {
             burgers: data
         };
+        console.log(hbsObject);
+        res.render("index", hbsObject)
     });
 
 });
@@ -20,3 +22,26 @@ router.post("/", function(req, res) {
         res.redirect("/");
     });
 });
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function() {
+      res.redirect("/");
+    });
+  });
+  
+  router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function() {
+      res.redirect("/");
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
