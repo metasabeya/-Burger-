4871a208f04d53b8889af567a0befae2c7cfa79c
unknown <metybuza46@gmail.com>
2020-09-19 17:59:15 -0600

    added new code to hooking the app with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,36 @@
 // MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "enguMY@2019",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'enguMY@2019',
+    database: 'todoagain_db'
+  })
+};
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "enguMY@2019",
+//   database: "burgers_db"
+// });
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  //console.log("connected as id: " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   //console.log("connected as id: " + connection.threadId);
+// });
 
 
-
+connection.connect();
 // Export connection for ORM 
 module.exports = connection;
